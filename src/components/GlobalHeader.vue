69eaa67c01a498af3435fcf862c904da4ff17211
Ayaka <1091608158@qq.com>
2024-01-08 19:03:29 +0800

    “init”

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.gif" />
+          <div class="title">LOJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path"
+        >{{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { routes } from "../router/routes";
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
