657fcb7575e91bd16b1130392990c2fd10bd238e
Ayaka <1091608158@qq.com>
2024-01-25 07:47:01 +0800

    完善管理题目页面

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,7 @@ import AdminView from "@/views/AdminView.vue";
 import NoAuthView from "@/views/NoAuthView.vue";
 import ACCESS_ENUM from "@/access/accessEnum";
 import AddQuestionView from "@/views/question/AddQuestionView.vue";
+import ManageQuestionView from "@/views/question/ManageQuestionView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
@@ -33,6 +34,14 @@ export const routes: Array<RouteRecordRaw> = [
     path: "/add/question",
     name: "创建题目",
     component: AddQuestionView,
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
+  },
+  {
+    path: "/manage/question/",
+    name: "管理题目",
+    component: ManageQuestionView,
     // meta: {
     //   access: ACCESS_ENUM.ADMIN,
     // },
